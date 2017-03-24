f62149bbbee19782f03e23d9dd6b99a08128305e
Thomas Cheng <thomascheng1998@googlemail.com>
2017-03-24 18:00:03 +0000

    Added code gen to emulate function dispatch but on an entity enum, so effectively a sized trait object

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -12,32 +12,49 @@ pub enum Entity {
   SplitterMan(SplitterMan),
 }
 
+macro_rules! empty {
+  () => {}
+}
+
+/// A macro to codegen a function dispatch.
+/// # Usages
+/// ## Codegen a function
+/// ### Params
+/// * `$ent`    - The entity to match.
+/// * `$func`   - The identifier of the function to call (i.e. `update`)
+/// * `$b_rule` - The borrowing rules for the match arms - i.e. ref mut or ref. 
+///               Needs to be surrounded by [], so [ref mut], or [ref].
+/// * `$params` - A list of optional parameters to pass to the method.
+/// ### Example
+/// ```
+/// entity_match_and_run!(*self, update, [ref mut], engine);
+/// ```
+macro_rules! entity_match_and_run {
+  ( $ent: expr, $func: ident, [$( $b_rule:tt )*] $(,$arg:tt)* ) => (
+    match $ent {
+      Entity::SplitterMan($($b_rule)* e) => e.$func($($arg)*)
+    }
+  );
+}
+
 impl Entity {
   /// Returns a tuple. 
   /// # 1: True if this entity should be removed after the update.
   /// # 2: A list of entities to add after the update.
   pub fn update(&mut self, engine: &Engine) -> (bool, Option<Vec<Entity>>) {
-    match *self {
-      Entity::SplitterMan(ref mut e) => e.update(engine),
-    }
+    entity_match_and_run!(*self, update, [ref mut], engine)
   }
 
   pub fn render(&self, cont: &mut RendererController) {
-    match *self {
-      Entity::SplitterMan(ref e) => e.render(cont),
-    }
+    entity_match_and_run!(*self, render, [ref], cont);
   }
 
   pub fn get_entity_handle(&self) -> Option<EHandle> {
-    match *self {
-      Entity::SplitterMan(ref e) => e.id,
-    }
+    entity_match_and_run!(*self, get_id, [ref])
   }
 
   pub fn set_entity_handle(&mut self, h: EHandle) {
-    match *self {
-      Entity::SplitterMan(ref mut e) => e.id = Some(h),
-    }
+    entity_match_and_run!(*self, set_id, [ref mut], h)
   }
 }
 
